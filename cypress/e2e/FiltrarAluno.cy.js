c56400bca10b462189f1ebd0009af43154192c3d
Lucas Ferreira Torres de Albuquerque <lfta@cesar.school>
2024-05-01 13:07:18 -0300

    teste e2e de filtros de alunos realizado com sucesso

--- cypress/e2e/FiltrarAluno.cy.js
+++ cypress/e2e/FiltrarAluno.cy.js
@@ -8,11 +8,18 @@ describe('historia de filtrar aluno', () => {
         cy.get('#senha').type(senha);
         cy.get('#botao').click();
         cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(1) > :nth-child(2)').invoke('text').as('name_first');
+
         cy.wait(2000); 
         cy.get('[onclick="sort(1)"]').click();
         cy.wait(2000);
         cy.get('[onclick="sort(1)"]').click();
         cy.wait(2000);
+
+        cy.get('@name_first').then((name_first) => {
+            cy.get('tbody > :nth-child(1) > :nth-child(2)').should('have.text', name_first);
+        });
     })
     it('filtrar alunos por serie/turma com sucesso', () => {
         cy.visit('/');
@@ -20,11 +27,18 @@ describe('historia de filtrar aluno', () => {
         cy.get('#senha').type(senha);
         cy.get('#botao').click();
         cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(1) > :nth-child(3)').invoke('text').as('class_first');
+
         cy.wait(2000); 
         cy.get('[onclick="sort(2)"]').click();
         cy.wait(2000); 
         cy.get('[onclick="sort(2)"]').click();
         cy.wait(2000);
+
+        cy.get('@class_first').then((class_first) => {
+            cy.get('tbody > :nth-child(1) > :nth-child(3)').should('have.text', class_first);
+        });
     })
     it('filtrar alunos por data de matricula com sucesso', () => {
         cy.visit('/');
@@ -32,11 +46,18 @@ describe('historia de filtrar aluno', () => {
         cy.get('#senha').type(senha);
         cy.get('#botao').click();
         cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(1) > :nth-child(4)').invoke('text').as('date_first');
+
         cy.wait(2000); 
         cy.get('[onclick="sort(3)"]').click();
         cy.wait(2000);
         cy.get('[onclick="sort(3)"]').click();
-        cy.wait(2000);  
+        cy.wait(2000);
+
+        cy.get('@date_first').then((date_first) => {
+            cy.get('tbody > :nth-child(1) > :nth-child(4)').should('have.text', date_first);
+        });
     })       
     it('filtrar alunos por id com sucesso', () => {
         cy.visit('/');
@@ -44,10 +65,18 @@ describe('historia de filtrar aluno', () => {
         cy.get('#senha').type(senha);
         cy.get('#botao').click();
         cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(1) > :nth-child(1)').invoke('text').as('id_first');
+
+
         cy.wait(2000); 
         cy.get('[onclick="sort(0)"]').click();
         cy.wait(2000);  
         cy.get('[onclick="sort(0)"]').click();
-        cy.wait(2000); 
+        cy.wait(2000);
+
+        cy.get('@id_first').then((id_first) => {
+            cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', id_first);
+        });
     })                                
 })          
