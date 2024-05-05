7373a41a4b009fb21606044a610f4397b449c1f9
França <143142740+FelipeARFranca@users.noreply.github.com>
2024-05-05 13:43:55 -0300

    Update AtualizarAluno.cy.js
    Testa mais combinações de preenchimento do formulário de edição.
    
    Testa se o formulário barra o usuário de tentar salvar as alterações deixando o campo de nome vazio.

--- cypress/e2e/AtualizarAluno.cy.js
+++ cypress/e2e/AtualizarAluno.cy.js
@@ -146,6 +146,125 @@ describe('atualizar aluno', () => {
         cy.get('.btn').click();
 
     })
+    it('atualizar aluno nome e serie', () => {
+        cy.visit('/');
+        cy.get('#nome').type(nome);
+        cy.get('#senha').type(senha);
+        cy.get('#botao').click();
+        cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(1) > :nth-child(1)').invoke('text').as('id');
+        cy.get('tbody > :nth-child(1) > :nth-child(4)').invoke('text').as('date');
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(1) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#nome-aluno').clear();
+        cy.get('#nome-aluno').type(new_name);
+        cy.get('#turma-aluno').select(new_class);
+        cy.get('.btn').click();
+        
+        cy.get('@id').then((id) => {
+            cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', id);
+            cy.get('tbody > :nth-child(1) > :nth-child(2)').should('have.text', new_name);
+            cy.get('tbody > :nth-child(1) > :nth-child(3)').should('have.text', new_class);
+        });
+        cy.get('@date').then((date) => {
+            cy.get('tbody > :nth-child(1) > :nth-child(4)').should('have.text', date);
+        });
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(1) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#nome-aluno').clear();
+        cy.get('#nome-aluno').type('123123123');
+        cy.get('#turma-aluno').select('1-A');
+        cy.get('.btn').click();
+        
+    })
+    it('atualizar aluno nome e data', () => {
+        cy.visit('/');
+        cy.get('#nome').type(nome);
+        cy.get('#senha').type(senha);
+        cy.get('#botao').click();
+        cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(2) > :nth-child(1)').invoke('text').as('id');
+        cy.get('tbody > :nth-child(2) > :nth-child(3)').invoke('text').as('class_num');
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(2) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#nome-aluno').clear();
+        cy.get('#nome-aluno').type(new_name);
+        cy.get('#data-aluno').click();
+        cy.get('#data-aluno').type(new_date);
+        cy.get('.btn').click();
+
+        cy.get('@id').then((id) => {
+            cy.get('tbody > :nth-child(2) > :nth-child(1)').should('have.text', id);
+            cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', new_name);
+            cy.get('tbody > :nth-child(2) > :nth-child(4)').should('have.text', new_date_check);
+        });
+
+        cy.get('@class_num').then((class_num) => {
+            cy.get('tbody > :nth-child(2) > :nth-child(3)').should('have.text', class_num);
+        });
+        
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(2) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#nome-aluno').clear();
+        cy.get('#nome-aluno').type('xico ciência'); 
+        cy.get('#data-aluno').click();
+        cy.get('#data-aluno').type('2024-04-26');
+        cy.get('.btn').click();
+
+    })
+    it('atualizar aluno serie e data', () => {
+        cy.visit('/');
+        cy.get('#nome').type(nome);
+        cy.get('#senha').type(senha);
+        cy.get('#botao').click();
+        cy.get('#aalunos').click();
+
+        cy.get('tbody > :nth-child(3) > :nth-child(1)').invoke('text').as('id');
+        cy.get(':nth-child(3) > :nth-child(2)').invoke('text').as('name');
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(3) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#turma-aluno').select(new_class);
+        cy.get('#data-aluno').click();
+        cy.get('#data-aluno').type(new_date);
+        cy.get('.btn').click();
+
+        cy.get('@id').then((id) => {
+            cy.get('tbody > :nth-child(3) > :nth-child(1)').should('have.text', id);
+            cy.get(':nth-child(3) > :nth-child(3)').should('have.text', new_class);
+            cy.get(':nth-child(3) > :nth-child(4)').should('have.text', new_date_check);
+        });
+        cy.get('@name').then((name) => {
+            cy.get(':nth-child(3) > :nth-child(2)').should('have.text', name);
+        });
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(3) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#turma-aluno').select('3-A');
+        cy.get('#data-aluno').click();
+        cy.get('#data-aluno').type('2004-08-04');
+        cy.get('.btn').click();
+    })
+    it('atualizar aluno nome não pode estar vazio', () => {
+        cy.visit('/');
+        cy.get('#nome').type(nome);
+        cy.get('#senha').type(senha);
+        cy.get('#botao').click();
+        cy.get('#aalunos').click();
+
+        cy.get('[onclick="editar()"]').click();
+        cy.get(':nth-child(1) > :nth-child(7) > .cadastrar-botao').click();
+        cy.get('#nome-aluno').clear();
+        cy.get('.btn').click();
+        cy.get('.cadastrar_aluno').should('be.visible');
+        
+    })
 
 
 })
